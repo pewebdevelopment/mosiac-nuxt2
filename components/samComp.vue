678061a84c7c42f739ca5d600f9550e4889aaef2
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-15 17:53:45 +0530

    Initial Commit of Nuxt 2 with Tailwid and TypeScript for the mosiac theme

---
+++ components/samComp.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    This is my Sample Component
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+    <div>
+      <h1 class="text-3xl font-bold underline">From inside the div</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <samComp />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import SamComp from "../components/samComp.vue";
+
+export default Vue.extend({
+  name: "IndexPage",
+});
+
+SamComp;
+</script>
