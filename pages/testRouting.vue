70ec8831c680e70eb16b76e9dcd1a6f0187fe604
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-17 15:17:48 +0530

    testroute.vue page working

---
+++ pages/testRouting.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex h-screen overflow-hidden">
+    <NuxtLink to="/">Home Page</NuxtLink>
+    <!-- Sidebar -->
+    <Sidebar :sidebarOpen="sidebarOpen" @close-sidebar="sidebarOpen = false" />
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import Sidebar from "../partials/SidebarNuxt.vue";
+import Header from "../partials/Header.vue";
+// import { useRouter } from "vue-router";
+
+export default {
+  name: "Inbox",
+  components: {
+    Sidebar,
+    Header,
+  },
+};
+</script>
+// const currentRoute = useRouter().currentRoute.value;
+// console.log(currentRoute);
