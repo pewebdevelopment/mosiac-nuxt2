35831fc1aa04a0ae49eb0457d90ecd430a2dda80
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-20 17:48:34 +0530

    Sidebar successfully imported in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     This is the Index.vue Page
-    <samComp />
+
+    <Sidebar
+      :sidebarOpen="sidebarOpen"
+      @close-sidebar="sidebarOpen.value = false"
+    />
 
     <div id="sliderSvg">
       <svg
@@ -6237,9 +6241,31 @@
 
 <script lang="ts">
 import Vue from "vue";
-// import SamComp from "@/components/samComp.vue";
+import { ref } from "vue";
+import Sidebar from "../partials/Sidebar.vue";
+import Header from "../partials/Header.vue";
 
 export default Vue.extend({
   name: "IndexPage",
+  components: {
+    Sidebar,
+    Header,
+  },
+  setup() {
+    const sidebarOpen = ref(false);
+
+    return {
+      sidebarOpen,
+    };
+  },
 });
 </script>
+
+
+
+
+
+export default {
+  name: "Analytics",
+  
+};
