7c6b2a075efd96367c57f9b3dcacba16e689af14
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-22 22:34:41 +0530

    Trying to fix the content area

--- pages/dashboard/Analytics.vue
+++ pages/dashboard/Analytics.vue
@@ -5,9 +5,11 @@
 
     <!-- Content area -->
     <div
+      id="content-area"
       class="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden"
     >
       <!-- Site header -->
+
       <Header
         :sidebarOpen="sidebarOpen"
         @toggle-sidebar="sidebarOpen = !sidebarOpen"
--- pages/dashboard/Fintech.vue
+++ pages/dashboard/Fintech.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex h-screen overflow-hidden">
+  <div class="flex h-screen overflow-hidden relative">
     <!-- Sidebar -->
     <Sidebar :sidebarOpen="sidebarOpen" @close-sidebar="sidebarOpen = false" />
 
