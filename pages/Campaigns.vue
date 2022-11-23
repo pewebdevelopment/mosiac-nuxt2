c099091c98ab7702bc94512601fcfd08acbfab00
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-23 15:31:43 +0530

    Fixed CSS Issues in Campaigns

--- pages/Campaigns.vue
+++ pages/Campaigns.vue
@@ -63,15 +63,7 @@
           </div>
 
           <!-- Cards -->
-          <div
-            class="
-              grid grid-cols-1
-              sm:grid-cols-2
-              md:grid-cols-3
-              lg:grid-cols-4
-              gap-6
-            "
-          >
+          <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
             <CampaignsCard v-for="item in items" :key="item.id" :item="item" />
           </div>
 
